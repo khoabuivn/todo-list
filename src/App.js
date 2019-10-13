da8edc33e8ccf8108c669016f8ec92f12c4702d4
Khoa Bui <khoabuivnhcm@gmail.com>
2019-10-13 21:35:56 +0700

    Clear completed, display by 3 filters, check all

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import TodoItem from './components/TodoItem';
 import checkall from './img/checkall.svg';
+import classnames from 'classnames';
 
 class App extends Component {
   constructor() {
@@ -9,7 +10,7 @@ class App extends Component {
   
     this.state = {
       newItem: "",
-      currentFilter: "all", // all, active
+      currentFilter: "All", // all, active
       todoItems: [
         { title: 'Self-learn ', isComplete: true },
         { title: 'Do family chores', isComplete: true},
@@ -17,7 +18,10 @@ class App extends Component {
       ]
     }
     this.onKeyUp = this.onKeyUp.bind(this);
-    this.onChange = this.onChange.bind(this)
+    this.onChange = this.onChange.bind(this);
+    this.changeFilter = this.changeFilter.bind(this);
+    this.checkAll = this.checkAll.bind(this);
+    this.clearCompleted = this.clearCompleted.bind(this)
   }
   
   onItemClick(item) {
@@ -49,6 +53,7 @@ class App extends Component {
   
       this.setState({
         newItem: "",
+        currentFilter: this.state.currentFilter,
         todoItems: [
           { title: text, isComplete: false},
           ...this.state.todoItems
@@ -64,9 +69,57 @@ class App extends Component {
     })
   }
 
+  changeFilter(event) {
+    const currentFilter = event.target.innerHTML;
+    this.setState({
+      newItem: this.state.newItem,
+      currentFilter: currentFilter,
+      todoItems: [
+        ...this.state.todoItems
+      ]
+    })
+  }
+
+  checkAll() {
+    const { todoItems } = this.state;
+
+    this.setState({
+      ...this.state,
+      todoItems: [
+        ...todoItems.map(function(item) {
+          return { title: item.title, isComplete: true}
+        })
+      ]
+    })
+  }
+
+  clearCompleted() {
+    this.setState({
+      ...this.state,
+      todoItems: [
+        ...this.state.todoItems.filter((item) => item.isComplete === false)
+      ]
+    })
+  }
+
   render() {
-    const { todoItems, newItem } = this.state;
+    const { todoItems, newItem, currentFilter } = this.state;
     let count =  todoItems.filter(item => item.isComplete === false).length;
+    let todoItemsFiltered = todoItems.filter(function(item) {
+      switch(currentFilter) {
+        case "Completed":
+          if(item.isComplete === true)
+          return item;
+          break;
+        case "Active":
+          if(item.isComplete === false)
+          return item;
+          break;
+        default: 
+          return item;
+          break;
+      }
+    })
     return (
       <div className="App">
         
@@ -75,7 +128,7 @@ class App extends Component {
       */}
       <div className="wrapper">
         <div className="Header">
-          <img src={checkall} width={32} alt="logo"/>
+          <img src={checkall} width={32} alt="logo" onClick={this.checkAll}/>
           <input 
             type="Text" 
             placeholder="Add an item" 
@@ -85,7 +138,7 @@ class App extends Component {
           />
         </div>
         {
-          todoItems.length > 0 && todoItems.map((item, index) => 
+          todoItemsFiltered.length > 0 && todoItemsFiltered.map((item, index) => 
             <TodoItem 
               key = {index} 
               item = {item} 
@@ -93,8 +146,8 @@ class App extends Component {
             />
           )
         }
-        {  todoItems.length === 0 && "Nothing here" }
-        
+       
+    
         <div className="footer">
           <span className="todo-count">
             {
@@ -106,17 +159,29 @@ class App extends Component {
             </span>
           <ul className="filters">
             <li>
-              <a className="selected">All</a>
+              <a className={classnames({
+                selected: currentFilter === "All"
+              })} onClick={this.changeFilter}>All</a>
             </li>
             <li>
-              <a>Active</a>
+              <a className={classnames({
+                selected: currentFilter === "Active"
+              })} onClick={this.changeFilter}>Active</a>
             </li>
             <li>
-              <a>Completed</a>
+              <a className={classnames({
+                selected: currentFilter === "Completed"
+              })} onClick={this.changeFilter}>Completed</a>
             </li>
           </ul>
-          <button className="clear-completed">Clear Completed</button>
+          <button className={classnames('clear-completed', {
+            active: todoItemsFiltered.find((item) => item.isComplete === true) 
+          })} onClick={this.clearCompleted}>Clear Completed</button>
         </div>
+  
+
+        {  todoItemsFiltered.length === 0 && "" }
+
       </div>
       </div>
     );
